839e53e2e47c89cf9d38ade6c598585cb14a4806
usausa <machi.pon@gmail.com>
2021-06-12 20:14:36 +0900

    Update project baseline

---
+++ Smart.Analyzers.JapaneseComment.Sample/TargetClass1.cs
@@ -0,0 +1,12 @@
+namespace Smart.Analyzers.JapaneseComment.Sample
+{
+    // あああ
+    public class TargetClass1
+    {
+        // aaa
+        public static void Test()
+        {
+            /* test */
+        }
+    }
+}
