6eb036158972e7ca3ba0a4bcd0b2319fff10b06d
usausa <machi.pon@gmail.com>
2021-11-20 13:23:40 +0900

    Update setting

--- Smart.Analyzers.JapaneseComment/RuleIdentifiers.cs
+++ Smart.Analyzers.JapaneseComment/RuleIdentifiers.cs
@@ -1,6 +1,5 @@
 namespace Smart.Analyzers.JapaneseComment;
 
-// ReSharper disable once InconsistentNaming
 internal static class RuleIdentifiers
 {
     public const string KanaCharacterInCommentShouldBeWide = "SAJ0001";
